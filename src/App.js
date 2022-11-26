319f51be3b6c5836c5876f2d6f9961da6114a9ba
ismael29h <ismael29h@gmail.com>
2022-11-26 18:11:19 -0300

    react-router-dom agregado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import Inicio from './paginas/Inicio'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Inicio />
+  },
+  {
+    path: "/buscador",
+    element: <Inicio />,
+  },
+]);
+
+
 
 function App() {
   return (
     <div className="App">
       {/* INICIO */}
-      <Inicio />
+      <RouterProvider router={router} />
     </div>
   );
 }
