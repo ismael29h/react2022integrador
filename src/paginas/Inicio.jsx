30ee109dbdc6b065b467d3452c55183c8678d8c6
ismael29h <ismael29h@gmail.com>
2022-11-26 18:47:48 -0300

    Probando y agregando funcionalidades a la página de inicio

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import Header from '../componentes/Header/Header';
-import Footer from '../componentes/Footer/Footer';
+import { useState } from "react";
+import { useEffect } from "react";
+import { getListadoNoticias } from "../servicios/noticias";
+import { useSearchParams } from "react-router-dom";
 
 const Inicio = () => {
+    const [noticias, setNoticias] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [pagina, setPagina] = useState(1);
+    const [cantidadPaginas, setCantidadPaginas] = useState(1)
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const buscarNoticias = async () => {
+        setIsLoading(true);
+        const {Search: noticia, totalResultados} = await getListadoNoticias(searchParams.get('query'), pagina);
+        setNoticias(noticia);
+        setCantidadPaginas(Math.ceil(parseInt(totalResultados)/10));
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        if (searchParams.get('query')) {
+            buscarNoticias(pagina)
+        }
+    }, [searchParams, pagina]);
+
+    const onBuscar = (criterioBusqueda) => {
+        setSearchParams({ query: criterioBusqueda });
+    };
+
+    const onCambioPagina = (pagina) => {
+        setPagina(pagina);
+    }
+
     return (
-        <div id="pagina">
-            <Header />
-            {/* CUERPO */}
-            <h1>Hola</h1>
-            <Footer />
-        </div>
+        <h1>Probando</h1>
     )
+
+
+
 
 };
 
