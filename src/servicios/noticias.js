a9328a2c494b53c470a2a84c1e5a854f50bb9d4b
ismael29h <ismael29h@gmail.com>
2022-11-26 11:36:40 -0300

    Algunas constantes agregadas (imagen, api_jey, etc)

---
+++ src/servicios/noticias.js
@@ -0,0 +1,11 @@
+const API_KEY = '3a4bc2fe11a04677b20d885746065d2d'
+const OMDB_API = 'https://newsapi.org'
+const PAGE_SIZE = '10'
+const LANGUAGE = 'es'
+
+
+export const getListadoNoticias = async (criterioBusqueda, paginaActual) => {
+    const respuesta = await fetch('${OMDB_API}/v2/everything?q=${criterioBusqueda}&apiKey=${API_KEY}&page=${paginaActual}&pageSize=${PAGE_SIZE}&language=${LANGUAGE}');
+    const noticias = await respuesta.json();
+    return noticias;
+}
